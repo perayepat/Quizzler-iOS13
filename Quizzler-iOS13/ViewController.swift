403c9dd560665c7a975eda8d18762fbf2453b9ed
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-06-19 08:14:10 +0200

    :notebook:  Progress bar working

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,48 +15,89 @@ class ViewController: UIViewController {
     @IBOutlet var trueButton: UIButton!
     @IBOutlet var falseButton: UIButton!
     
-    let quiz = [
-        ["Two is a prime number","True"],
-        ["Hoe is a gardening tool","True"],
-        ["I like reading", "True"]
-    ]
+    let totalQuestions = quiz.count
     
-    var questionNumber = 0;
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
             // Do any additional setup after loading the view.
         updateUi()
+        progressBar.progress = 0
     }
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        ///angela way
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber].answer
         
-        
-        
-        if questionNumber == quiz.count{
-
-            questionNumber = 0
-            updateUi()
-        }
-        else{
-
-            let userAns = sender.currentTitle!
-            let actualAns = quiz[questionNumber][1]
+        if userAnswer == actualAnswer{
+            sender.backgroundColor = UIColor.green
             
-            if(userAns == actualAns){
-                print("Your right")
-            }
-            else{
-                print("Your Wrong")
-            }
-            updateUi()
+            
+        } else {
+            sender.backgroundColor = UIColor.red
+            
         }
         
-        questionNumber += 1
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+            progressBar.progress = 0
+        }
+       
+        
+        /// Two ways to delay the updateUI
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.updateUi()
+        }
+       
+//        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+      
+        ///My way
+//
+//        if questionNumber == quiz.count{
+//
+//            questionNumber = 0
+//            updateUi()
+//        }
+//        else{
+//
+//            let userAns = sender.currentTitle!
+//            let actualAns = quiz[questionNumber][1]
+//
+//            if(userAns == actualAns){
+//                print("Your right")
+//            }
+//            else{
+//                print("Your Wrong")
+//            }
+//            updateUi()
+//        }
+//
+//        questionNumber += 1
    
     }
     
     func updateUi(){
-        questionLabel.text = quiz[questionNumber][0]
+//        questionLabel.text = quiz[questionNumber][0]
+        let progressPercentage = Float(questionNumber)/Float(totalQuestions)
+        progressBar.setProgress(progressPercentage, animated: true)
+        questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        
+    }
+    
+    //Using a Timer
+    @objc func updateUI(){
+        let progressPercentage = Float(questionNumber)/Float(totalQuestions)
+        progressBar.setProgress(progressPercentage, animated: true)
+        questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
     
 }
